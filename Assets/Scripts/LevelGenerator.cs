1b16516fca673dbbd5e0db8238f801a66b30b9c2
Alexey Dmitriev <alex717nd@gmail.com>
2025-09-03 15:21:49 -0700

    Varying asteroid size in map generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [Header("Map Generation")]
     [SerializeField] private Vector2 mapSize;
     [SerializeField] private float generationSteps;
     [SerializeField] private float scale;
     [SerializeField] private float asteroidSpawnThreshold;
-    [SerializeField] private GameObject asteroidPrefab;
     [SerializeField] private float minSpacing;
 
-    private Vector2 noiseOffset;
+    [Header("Asteroid Config")]
+    [SerializeField] private GameObject asteroidPrefab;
+    [SerializeField] private AnimationCurve asteroidSizeDestribution;
+    [SerializeField] private Vector2 asteroidSizeRange;
 
+    [Header("Initial Settings")]
+    [SerializeField] private Transform asteroidParent;
+
+    private Vector2 noiseOffset;
     private Dictionary<GameObject, float> placedAsteroids = new Dictionary<GameObject, float>();
 
     private void Start()
     {
         // Generate perlin noise for level generation
-        float startGenTime = Time.time;
-        Debug.Log("Starting generation");
+        float startGenTime = Time.realtimeSinceStartup;
 
         noiseOffset = new Vector2(Random.Range(0f, 9999f), Random.Range(0f, 9999f));
 
@@ -28,39 +34,40 @@ public class LevelGenerator : MonoBehaviour
             GenerateAsteroid();
         }
 
-        float endGenTime = Time.time;
-        Debug.Log(startGenTime);
-        Debug.Log(endGenTime);
-        Debug.Log("Generation Finished; Took " + (endGenTime - startGenTime).ToString());
+        float endGenTime = Time.realtimeSinceStartup;
+        Debug.Log("Map Generation Finished; Took " + (endGenTime - startGenTime).ToString());
     }
 
     private void GenerateAsteroid()
     {
         float x = Random.Range(-mapSize.x / 2f, mapSize.x / 2f);
         float y = Random.Range(-mapSize.y / 2f, mapSize.y / 2f);
-        float perlinNoise = FractalNoise(x, y);
+        float perlinNoise = PerlinNoise(x, y);
         Vector2 spawnPosition = new Vector2(x, y);
-        Debug.Log(x.ToString() + " " + y.ToString() + " " + perlinNoise.ToString());
-
+        
         if (perlinNoise < asteroidSpawnThreshold)
             return;
+
+        float asteroidRadius = Mathf.Lerp(asteroidSizeRange.x, asteroidSizeRange.y, asteroidSizeDestribution.Evaluate(Random.value));
 
         // Avoid Collision with Other Asteroid
         foreach (KeyValuePair<GameObject, float> entry in placedAsteroids)
         {
-            GameObject asteroidObj = entry.Key;
-            float asteroidRadius = entry.Value;
-            if (Vector2.Distance(spawnPosition, asteroidObj.transform.position) < minSpacing + asteroidRadius * 2)
+            GameObject spawnedAsteroidObj = entry.Key;
+            float spawnedAsteroidRadius = entry.Value;
+            if (Vector2.Distance(spawnPosition, spawnedAsteroidObj.transform.position) < minSpacing + spawnedAsteroidRadius + asteroidRadius)
                 return;
         }
 
         // Spawn Asteroid
         GameObject asteroid = Instantiate(asteroidPrefab, spawnPosition, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        asteroid.transform.localScale = Vector2.one * asteroidRadius;
+        asteroid.transform.parent = asteroidParent;
 
-        placedAsteroids.Add(asteroid, .505f);
+        placedAsteroids.Add(asteroid, asteroidRadius);
     }
 
-    private float FractalNoise(float x, float y)
+    private float PerlinNoise(float x, float y)
     {
         float noise = Mathf.PerlinNoise((x + noiseOffset.x) / scale, (y + noiseOffset.y) / scale);
         return noise;
