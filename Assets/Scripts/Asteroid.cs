a0751944bf205deff2f3b5707eb2badbaab914ca
Alexey Dmitriev <alex717nd@gmail.com>
2025-09-10 13:42:53 -0700

    Asteroid Destroy Particle Effect

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour {
 
     private float health;
-
     private const float healthConst = 4;
+    private const float particleMult = 100;
+
+    [SerializeField] private GameObject destroyEffect;
 
     private void Start()
     {
@@ -22,6 +24,15 @@ public class Asteroid : MonoBehaviour {
 
     private void OnDestroy()
     {
-        // TODO add destroy effect
+        if (destroyEffect != null)
+        {
+            GameObject effect = Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            ParticleSystem particleSystem = effect.GetComponent<ParticleSystem>();
+            ParticleSystem.Burst burst = particleSystem.emission.GetBurst(0);
+            burst.count = new ParticleSystem.MinMaxCurve(Mathf.Round(particleMult * transform.localScale.x));
+            particleSystem.emission.SetBurst(0, burst);
+            ParticleSystem.ShapeModule shape = particleSystem.shape;
+            shape.radius *= transform.localScale.x;
+        }
     }
 }
